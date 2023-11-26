39c98676b06feb9daefe7b26364ad6f0d475c823
bhanu102 <bhanugadupudi15@gmail.com>
2023-11-26 17:29:48 -0600

    updated settings.py

--- doccure/settings.py
+++ doccure/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOCAL_FILE_DIR = os.path.join(BASE_DIR, 'doccure')
